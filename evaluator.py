acf0ede49e8936a8157f85e1f699edad63c336fc
mth-1012 <mth.1012.ta@gmail.com>
2022-06-21 19:25:41 -0400

    added dataset imagefolders

--- evaluator.py
+++ evaluator.py
@@ -2,11 +2,12 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torchvision.datasets
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import plot_confusion_matrix
 from skorch.helper import SliceDataset
 from sklearn.model_selection import cross_val_score
-from main import import_datasets
+from main import import_datasets, transform
 
 
 def predict_eval(net, dataset):
@@ -30,21 +31,28 @@ if __name__ == '__main__':
     print('COMP 472 Project')
     print('Evaluator')
 
-    """Device to evaluate"""
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cpu')
+    """Check device to evaluate"""
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cpu')
 
-    """Get random test dataset"""
+    """Get datasets"""
     data, _, test_dataset = import_datasets()
+
+    """Import bias datasets"""
+    male_dataset = torchvision.datasets.ImageFolder(root='./bias/gender/male/', transform=transform)
+    female_dataset = torchvision.datasets.ImageFolder(root='./bias/gender/female/', transform=transform)
+    child_dataset = torchvision.datasets.ImageFolder(root='./bias/age/child/', transform=transform)
+    adult_dataset = torchvision.datasets.ImageFolder(root='./bias/age/adult/', transform=transform)
+    senior_dataset = torchvision.datasets.ImageFolder(root='./bias/age/senior', transform=transform)
 
     """Reload model"""
     with open('model-pkl.pkl', 'rb') as f:
         net_reload = pickle.load(f)
         print('\nModel loaded')
 
-    """Predict"""
+    """Evaluate performance"""
     predict_eval(net_reload, test_dataset)
 
-    """K-fold Cross-Validation"""
+    """K-fold Cross-Validate"""
     k_fold_cross_validation(net_reload, data, k=5)
 
