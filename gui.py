4c37087f4fae4ee6c44a5d59e6e480d52fd97c88
linchen2508 <tuanlam80@gmail.com>
2022-07-20 17:17:40 -0400

    Make gui functional

--- gui.py
+++ gui.py
@@ -1,16 +1,19 @@
 import pickle
-import matplotlib.pyplot as plt
 import numpy as np
-import torch
 import torchvision.datasets
 import torchvision.transforms as transforms
-from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import ConfusionMatrixDisplay
-from skorch.helper import SliceDataset
-from sklearn.model_selection import cross_validate
 import PySimpleGUI as sg
 import os.path
-from PIL import Image
+import tensorflow as tf
+
+def model_evaluate_single(model, image_dir, transform, classes):
+    print('==== single evaluate ====')
+    image = tf.keras.preprocessing.image.load_img(image_dir)
+    image = transform(image)
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch.
+    predictions = model.predict(input_arr)
+    print(predictions)
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -19,35 +22,6 @@ transform = transforms.Compose([
     transforms.CenterCrop(32),
 ])
 classes = ('cloth', 'n95', 'none', 'surgical')
-
-
-def predict_eval(net, dataset, name):
-    print('\n==== Predict ====')
-    y_predict = net.predict(dataset)
-    y_test = np.array([y for x, y in iter(dataset)])
-    print('Accuracy for {} Dataset: {}%'.format(name, round(accuracy_score(y_test, y_predict) * 100, 2)))
-    ConfusionMatrixDisplay.from_predictions(y_test, y_predict, display_labels=classes)
-    plt.title('Confusion Matrix for {} Dataset'.format(name))
-    plt.show()
-
-
-def k_fold_cross_validation(net, dataset, k=5):
-    print('\n==== K-fold ====')
-    print('Warning: This takes approximately an hour or more!')
-    scoring = {
-        'accuracy': make_scorer(accuracy_score),
-        'precision': make_scorer(precision_score, average='weighted'),
-        'recall': make_scorer(recall_score, average='weighted'),
-        'f1_score': make_scorer(f1_score, average='weighted'),
-    }
-    y_train = np.array([np.int64(y) for x, y in iter(dataset)])
-    train_sliceable = SliceDataset(dataset)
-    scores = cross_validate(net, train_sliceable, y_train, cv=k, scoring=scoring)
-    print('\nScores:')
-    print('Accuracy: ', format(scores['test_accuracy']))
-    print('Precision: ', format(scores['test_precision']))
-    print('Recall: ', format(scores['test_recall']))
-    print('F1-measure: ', format(scores['test_f1_score']))
 
 if __name__ == '__main__':
     print('COMP 472 Project')
@@ -96,7 +70,7 @@ if __name__ == '__main__':
                 f
                 for f in file_list
                 if os.path.isfile(os.path.join(folder, f))
-                   and f.lower().endswith((".png", ".gif", ".jpg"))
+                   and f.lower().endswith((".png", ".gif", ".jpg", ".webp"))
             ]
             window["-FILE LIST-"].update(fnames)
         elif event == "-FILE LIST-":  # A file was chosen from the listbox
@@ -106,19 +80,22 @@ if __name__ == '__main__':
                     values["-FOLDER-"], values["-FILE LIST-"][0]
                 )
                 window["-TOUT-"].update(filename)
-                window["-IMAGE-"].update(filename=filename)
                 window.close()
             except:
                 pass
-    """Check device to evaluate"""
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    print(filename)
     """Get datasets"""
     data = torchvision.datasets.ImageFolder(root='./data/dataset/', transform=transform)
 
     """Reload model"""
     with open('model.pkl', 'rb') as f:
-        net_reload = pickle.load(f)
+        model = pickle.load(f)
         print('\nModel loaded')
+
+    model_evaluate_single(model, filename, transform, classes)
+
+
+
 
 
