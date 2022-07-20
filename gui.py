06e054946fbd9ab3128682d20c05ba7b2bd89bd7
linchen2508 <tuanlam80@gmail.com>
2022-07-20 14:25:31 -0400

    Move gui to a new file and restore original evaluator.py

--- gui.py
+++ gui.py
@@ -8,6 +8,9 @@ from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall
 from sklearn.metrics import ConfusionMatrixDisplay
 from skorch.helper import SliceDataset
 from sklearn.model_selection import cross_validate
+import PySimpleGUI as sg
+import os.path
+from PIL import Image
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -46,11 +49,67 @@ def k_fold_cross_validation(net, dataset, k=5):
     print('Recall: ', format(scores['test_recall']))
     print('F1-measure: ', format(scores['test_f1_score']))
 
-
 if __name__ == '__main__':
     print('COMP 472 Project')
     print('Extra Evaluator')
+    file_list_column = [
+        [
+            sg.Text("Image Folder"),
+            sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse(),
+        ],
+        [
+            sg.Listbox(
+                values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+            )
+        ],
+    ]
 
+    image_viewer_column = [
+        [sg.Text("Choose an image from list on left:")],
+        [sg.Text(size=(40, 1), key="-TOUT-")],
+        [sg.Image(key="-IMAGE-")],
+    ]
+
+    layout = [
+        [
+            sg.Column(file_list_column),
+            sg.VSeperator(),
+            sg.Column(image_viewer_column),
+        ]
+    ]
+
+    window = sg.Window("Image Viewer", layout)
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                # Get list of files in folder
+                file_list = os.listdir(folder)
+            except:
+                file_list = []
+
+            fnames = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(folder, f))
+                   and f.lower().endswith((".png", ".gif", ".jpg"))
+            ]
+            window["-FILE LIST-"].update(fnames)
+        elif event == "-FILE LIST-":  # A file was chosen from the listbox
+            try:
+                #Path to the chosen file
+                filename = os.path.join(
+                    values["-FOLDER-"], values["-FILE LIST-"][0]
+                )
+                window["-TOUT-"].update(filename)
+                window["-IMAGE-"].update(filename=filename)
+                window.close()
+            except:
+                pass
     """Check device to evaluate"""
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # device = torch.device('cpu')
@@ -70,12 +129,12 @@ if __name__ == '__main__':
         net_reload = pickle.load(f)
         print('\nModel loaded')
 
-    """Evaluate performance"""
-    # predict_eval(net_reload, male_dataset, 'Male (Gender)')
-    # predict_eval(net_reload, female_dataset, 'Female (Gender)')
-    # predict_eval(net_reload, child_dataset, 'Child (Age)')
-    # predict_eval(net_reload, adult_dataset, 'Adult (Age)')
-    # predict_eval(net_reload, senior_dataset, 'Senior (Age)')
-
-    """K-fold Cross-Validate"""
-    k_fold_cross_validation(net_reload, data, k=10)
+    # """Evaluate performance"""
+    # # predict_eval(net_reload, male_dataset, 'Male (Gender)')
+    # # predict_eval(net_reload, female_dataset, 'Female (Gender)')
+    # # predict_eval(net_reload, child_dataset, 'Child (Age)')
+    # # predict_eval(net_reload, adult_dataset, 'Adult (Age)')
+    # # predict_eval(net_reload, senior_dataset, 'Senior (Age)')
+    #
+    # """K-fold Cross-Validate"""
+    # k_fold_cross_validation(net_reload, data, k=10)
